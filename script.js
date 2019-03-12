3f95de38055efa3ac413d1071c3e9b826638a437
FieryRider <30357809+ivailogeimara@users.noreply.github.com>
2019-03-12 21:14:20 +0200

    Added filter/search (country and team is the same thing)

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-var matchesInfo;
-var matchesTable = document.getElementById("result");
+const filterBar = document.querySelectorAll('#search > input');
+var matchesTable = document.querySelector('#result > tbody');
+var matchesInfo = []
+var filteredMatchesInfo = []
+
+filterBar.forEach((filterField) => {
+    filterField.addEventListener('keyup', function(ev) {
+        filteredMatchesInfo = []
+        matchesInfo.forEach((el) => {
+            let countryFilter = document.getElementById('country-field').value.toLowerCase();
+            let teamNameFilter = document.getElementById('team-name-field').value.toLowerCase();
+            let cornersFilter = document.getElementById('corners-field').value.toLowerCase();
+            let passesFilter = document.getElementById('passes-field').value.toLowerCase();
+            let clearancesFilter = document.getElementById('clearances-field').value.toLowerCase();
+            let yelloCardsFilter = document.getElementById('yellow-cards-field').value.toLowerCase();
+            let redCardsFilter = document.getElementById('red-cards-field').value.toLowerCase();
+            let tacticsFilter = document.getElementById('tactics-field').value.toLowerCase();
+
+            let countryMatches = (el['home_team_country'].toLowerCase().indexOf(countryFilter) != -1) || (el['away_team_country'].toLowerCase().indexOf(countryFilter) != -1) 
+            let teamNameMatches = countryMatches;
+            let cornersMatches = (cornersFilter == "") || ((el['home_team_statistics']['corners'] == cornersFilter) || (el['away_team_statistics']['corners'] == cornersFilter))
+            let passesMatches = (passesFilter == "") || ((el['home_team_statistics']['num_passes'] == passesFilter) || (el['away_team_statistics']['num_passes'] == passesFilter))
+            let clearancesMatches = (clearancesFilter == "") || ((el['home_team_statistics']['clearances'] == clearancesFilter) || (el['away_team_statistics']['clearances'] == clearancesFilter))
+            let yelloCardsMatches = (yelloCardsFilter == "") || ((el['home_team_statistics']['yellow_cards'] == yelloCardsFilter) || (el['away_team_statistics']['yellow_cards'] == yelloCardsFilter))
+            let redCardsMatches = (redCardsFilter == "") || ((el['home_team_statistics']['red_cards'] == redCardsFilter) || (el['away_team_statistics']['red_cards'] == redCardsFilter))
+            let tacticsMatches = (tacticsFilter == "") || ((el['home_team_statistics']['tactics'] == tacticsFilter) || (el['away_team_statistics']['tactics'] == tacticsFilter))
+
+            let matchMatchesFilter = countryMatches && teamNameMatches && cornersMatches && 
+                passesMatches && clearancesMatches && yelloCardsMatches && redCardsMatches && tacticsMatches;
+
+            if (matchMatchesFilter)
+                filteredMatchesInfo.push(el);
+        });
+        displayMatchesInformation(filteredMatchesInfo);
+    });
+
+});
 
 Ajax.get('http://worldcup.sfg.io/matches/', function(response) {
     matchesInfo = response;
-    displayMatchesInformation();
+    displayMatchesInformation(matchesInfo);
 });
 
-function displayMatchesInformation() {
-    console.log(matchesInfo)
+
+function displayMatchesInformation(matchesInfo) {
+    clearMatchesTable();
     matchesInfo.forEach((el) => {
         let result = {
             'homeTeam': el['home_team']['goals'],
@@ -62,3 +98,8 @@ function addMatchToMatchesTable(matchInfo) {
     matchesTable.appendChild(row);
 }
 
+function clearMatchesTable() {
+    let newTbody = document.createElement('tbody');
+    matchesTable.parentNode.replaceChild(newTbody, matchesTable);
+    matchesTable = newTbody;
+}
